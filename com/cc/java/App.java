cac4631f5c5ef94599d6bc72fe42b1228d7ddb34
bjick84 <bjick@web.de>
2023-02-06 12:42:46 +0100

    areaSum++

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -17,10 +17,17 @@ public class App {
         output("Diff. r-c:" + areaDiff(rectangle, circle));
         output("Diff. c-t: " + areaDiff(circle, triangle));
         output("Diff. t-t: " + areaDiff(triangle, triangle));
+        output("------ Summen ------");
+        output("Diff. r-t: " + areaSum(rectangle, triangle));
+        output("Diff. r-c:" + areaSum(rectangle, circle));
+
     }
     
     private static double areaDiff(Shape s1, Shape s2) {
         return s1.area() - s2.area();
+    }
+    private static double areaSum(Shape s1, Shape s2) {
+        return s1.area() + s2.area();
     }
     private static void output(String string) {
         System.out.println(string);
