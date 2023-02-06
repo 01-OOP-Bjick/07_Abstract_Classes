e5b17f8aa2b180627b3ec6f51e0666806a336aa1
bjick84 <bjick@web.de>
2023-02-06 12:39:49 +0100

    Polymorphie++

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -7,18 +7,21 @@ public class App {
         Rectangle rectangle = new Rectangle(10.0, 10.0);
         Circle circle = new Circle(5.0);
         Triangle triangle = new Triangle(10.0, 10.0);
-        
+
         output("------ Flächen ------");
         output("Fläche Rechteck: " + rectangle.area());
         output("Fläche Dreieck: " + triangle.area());
         output("Fläche Kreis: " + circle.area());
         output("------ Differenzen ------");
         output("Diff. r-t: " + areaDiff(rectangle, triangle));
+        output("Diff. r-c:" + areaDiff(rectangle, circle));
+        output("Diff. c-t: " + areaDiff(circle, triangle));
+        output("Diff. t-t: " + areaDiff(triangle, triangle));
     }
-    private static double areaDiff(Rectangle r, Triangle t) {
-        return r.area() - t.area();
+    
+    private static double areaDiff(Shape s1, Shape s2) {
+        return s1.area() - s2.area();
     }
-
     private static void output(String string) {
         System.out.println(string);
     }
