ac88593824fdd0035db383076c62c28b6ab9038b
bjick84 <bjick@web.de>
2023-02-06 12:08:30 +0100

    alle Rechenarten++ abstrakte methoden++

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,11 +3,23 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+        
+        Rectangle rectangle = new Rectangle(5, 6);
+        Circle circle = new Circle(7);
+        Triangle triangle = new Triangle(4, 3);
+        
+        pollObj(rectangle);
+        pollObj(circle);
+        pollObj(triangle);
     }
 
-    private static void output(String outputStr) {
-        System.out.println(outputStr);
+    private static void pollObj(Shape area) {
+        output(area);
+    }
+
+
+    private static void output(Shape area) {
+        System.out.println(area);
     }
 
 
